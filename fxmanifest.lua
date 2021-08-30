38f3b337bb28ac2739dd9b7e0a34cea807bbcf0f
Toni Morton <haxor4evr@hotmail.com>
2021-08-30 06:00:40 -0700

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'gta5'
+
+name "TrafficAlert"
+description 'A simple Message Broadcast Script!'
+author "ToniMorton#3472"
+
+client_script 'client.lua'
+server_script 'server.lua'
